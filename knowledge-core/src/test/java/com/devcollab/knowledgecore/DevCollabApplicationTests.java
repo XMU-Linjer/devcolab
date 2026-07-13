f814b5752c56f903bd4c2e3a8c6192b6ab6d5bd7
Linker (XMU-Linjer) <884917500@qq.com>
2026-07-13 16:02:05 +0800

    chore: 初始化 Java 21 Maven 多模块工程

---
+++ knowledge-core/src/test/java/com/devcollab/knowledgecore/DevCollabApplicationTests.java
@@ -0,0 +1,9 @@
+package com.devcollab.knowledgecore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class DevCollabApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
